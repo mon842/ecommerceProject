24f32b5d12c1c0deddd4312670396be6bb587689
mon842 <sayakbasak842@gmail.com>
2023-08-19 16:16:02 +0530

    version 2.1
    + refactoring code  frontend

--- ecommerce-admin/components/ProductForm.js
+++ ecommerce-admin/components/ProductForm.js
@@ -8,12 +8,13 @@ const ProductForm = ({
   title: existingTitle,
   description: existingDescription,
   price: existingPrice,
-  images
+  images: existingImages,
 }) => {
 
   const [title, setTitle] = useState(existingTitle || '');
   const [description, setDescription] = useState(existingDescription || '');
   const [price, setPrice] = useState(existingPrice || '');
+  const [images, setImages] = useState(existingImages || []);
   const [goToProducts, setGoToProducts] = useState(false);
 
   // console.log(_id);
@@ -39,20 +40,23 @@ const ProductForm = ({
   }
 
   async function uploadImages(e) {
-    const files= e.target?.files;
-    if(files?.length>0){
-      const data= new FormData();
+    const files = e.target?.files;
+    if (files?.length > 0) {
+      const data = new FormData();
 
-      for(const file of files){
-        data.append('file',file);
+      for (const file of files) {
+        data.append('file', file);
       }
 
       // const res=await fetch('/api/upload', {
       //   method: 'POST',
       //   body: data
       // })
-      const res=await axios.post('/api/upload',data);
-      console.log(res.data);
+      const res = await axios.post('/api/upload', data)
+      console.log(res);
+      setImages((oldImages) => {
+        return [...oldImages, ...res.data.links];
+      })
     }
   }
 
@@ -68,7 +72,16 @@ const ProductForm = ({
       />
 
       <label htmlFor="">photos</label>
-      <div className='mb-2 '>
+      <div className='mb-2 flex flex-wrap gap-2'>
+        {!!images?.length && (
+          images.map((link) => (
+            <div key={link} className='h-20'>
+              <img className='h-20 rounded' src={link} alt="" />
+            </div>
+
+          ))
+        )}
+
         <label className='w-24 h-24 cursor-pointer text-center flex flex-col items-center justify-center text-sm gap-1 text-primary rounded-sm bg-white shadow-sm border border-primary'>
           <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
             <path strokeLinecap="round" strokeLinejoin="round" d="M3 16.5v2.25A2.25 2.25 0 005.25 21h13.5A2.25 2.25 0 0021 18.75V16.5m-13.5-9L12 3m0 0l4.5 4.5M12 3v13.5" />
@@ -78,7 +91,7 @@ const ProductForm = ({
             upload
           </div>
 
-          <input type="file" onChange={uploadImages} className='hidden'/>
+          <input type="file" onChange={uploadImages} className='hidden' />
         </label>
 
         {!images?.length && (
@@ -86,6 +99,8 @@ const ProductForm = ({
             No photos of product
           </div>
         )}
+
+
       </div>
 
       <label htmlFor="">description</label>
