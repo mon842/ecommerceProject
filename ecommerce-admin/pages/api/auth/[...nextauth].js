4fe0969fdbf03a5c8a2497843df8f56f4aac6e13
mon842 <sayakbasak842@gmail.com>
2023-10-01 15:57:21 +0530

    Update [...nextauth].js

--- ecommerce-admin/pages/api/auth/[...nextauth].js
+++ ecommerce-admin/pages/api/auth/[...nextauth].js
@@ -3,7 +3,7 @@ import GoogleProvider from 'next-auth/providers/google'
 import {MongoDBAdapter} from "@next-auth/mongodb-adapter";
 import clientPromise from '@/lib/mongodb';
 
-const arr=['siaassshane@gmail.com'];
+const arr=['siaassshane@gmail.com','sayakbasak842@gmail.com','sayakbasakhack@gmail.com'];
 
 export const authOptions = {
   providers: [
