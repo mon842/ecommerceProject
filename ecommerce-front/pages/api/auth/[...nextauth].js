45a2b30be4563b69700e0b5a79d6c35119e8e68a
mon842 <sayakbasak842@gmail.com>
2024-05-19 18:14:24 +0530

    hide

--- ecommerce-front/pages/api/auth/[...nextauth].js
+++ ecommerce-front/pages/api/auth/[...nextauth].js
@@ -13,7 +13,7 @@ export const authOptions = {
       clientSecret: process.env.GOOGLE_SECRET
     }),
   ],
-  secret:process.env.SECRET,
+  secret:process.env.NEXTAUTH_SECRET,
   adapter: MongoDBAdapter(clientPromise),
 
   callbacks: {
